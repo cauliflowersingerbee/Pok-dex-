88293209f79ca5911e2fa7bcddaf122eb5b792d6
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2021-07-31 11:15:29 +0200

    added Pokémon objects to array

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Pok-dex-
+A small web application with HTML, CSS, and JavaScript that loads
+data from an external API and enables the viewing of data points in detail.
---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+
+/* how to write arrays:
+
+1. array of strings
+let foodArray = ['pizza', 'tuna', 'apple'];
+
+2. array of objects
+let carArray = [
+  { type: 'Bus', wheels: 4, color: 'blue'},
+  { type: 'Sport', wheels: 4, color: 'red'}
+];
+
+3. array of arrays
+let myCalculatorNumbers = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+*/
+
+let pokemonList = [
+    { name: 'Bulbasaur', height: '0.7 m', weight: 6.9, types: ['grass', 'poison']},
+    { name: 'Ivysaur', height: '1 m', weight: 13, types: ['grass', 'poison']},
+    { name: 'Venusaur', height: '2 m', weight: 100, types: ['grass', 'poison']},
+    { name: 'Charmander', height: '0.6 m', weight: 8.5, types: 'fire'},
+    { name: 'Charmeleon', height: '1.1 m', weight: 19, types: 'fire'},
+];
+
+
+
+
+
+
+
+
+
+
